2d78a72e9d4a9261e8f05287fb191c2b94315205
rms rob <rr.rmsrob@gmail.com>
2023-02-01 05:17:19 +0100

    install

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,30 @@
 # !/usr/bin/env bash
-cp ~/.bashrc{,.bak} ~/.profile{,.bak}
 
 DOTFILES=/home/$USER/Repos/github.com/$USER/dotfiles
 chmod +x $DOTFILES/.local/scripts/*
 # chmod +x $HOME/.local/scripts_private/*
 
-ln -sf $DOTFILES/.bashrc $HOME/.bashrc
-ln -sf $DOTFILES/.profile $HOME/.profile
-ln -sf $DOTFILES/.inputrc $HOME/.inputrc
-ln -sf $DOTFILES/.dircolors $HOME/.dircolors
-ln -sf $DOTFILES/.tmux.conf $HOME/.tmux.conf
-ln -sf $DOTFILES/.vim/vim.vim $HOME/.vimrc
-ln -nsvf $DOTFILES/.vim $HOME
-ln -nsvf $DOTFILES/.config/alacritty $HOME/.config
-ln -nsvf $DOTFILES/.config/lynx $HOME/.config
-ln -nsvf $DOTFILES/.config/kitty $HOME/.config
-ln -nsvf $DOTFILES/shared/nvim $HOME/.config
+declare -a links=(
+    ".bashrc $HOME/.bashrc"
+    ".profile $HOME/.profile"
+    ".inputrc $HOME/.inputrc"
+    ".dircolors $HOME/.dircolors"
+    ".tmux.conf $HOME/.tmux.conf"
+    ".vim/vim.vim $HOME/.vimrc"
+    ".vim $HOME"
+    ".config/alacritty $HOME/.config"
+    ".config/lynx $HOME/.config"
+    ".config/kitty $HOME/.config"
+    # ".config/nvim $HOME/.config"
+)
+
+for link in "${links[@]}"; do
+    IFS=' ' read -r source target <<<"$link"
+    if [ -e "$target" ]; then
+        mv "$target" "$target.bak"
+    fi
+    ln -nsvf "$DOTFILES/$source" "$target"
+done
 
 touch ~/.env.example
 chmod 700 ~/.env.example
